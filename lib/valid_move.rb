f15b3f62aedbd3a897749e235020c93817e64fa9
Leifur <leifurerik@gmail.com>
2018-09-26 23:44:51 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -14,6 +14,8 @@ end
 def position_taken?(board, index)
   if ((board[index] == "") || (board[index] == " ") || (board[index] == nil))
     true
+  elsif index > 9 
+    false
   else
     false
   end
