7d2b4adc5f5e2b66854ad21c7f3886ddeb19ba28
Leifur <leifurerik@gmail.com>
2018-09-26 23:50:34 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -28,7 +28,7 @@ end
 #       return true
 #     end
 #   end
-# 
+#
 #   def on_board?(num)
 #     if num.between?(0, 8) == true
 #       return true
@@ -36,12 +36,12 @@ end
 #       return false
 #     end
 #   end
-# 
+#
 #   if (position_taken?(board, index)) == false && (on_board?(index) == true)
 #     return true
 #   else
 #     return false
 #   end
-# 
-# 
+#
+#
 # end
